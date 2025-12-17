975fc72bb5ef136f8ae2658c938effa9b7748995
Santhosh NC <ncsanthosh7@gmail.com>
2025-12-17 12:10:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const webdriver = require('selenium-webdriver');
 
 const username = process.env.BROWSERSTACK_USERNAME;
@@ -7,7 +6,11 @@ const buildName = process.env.BROWSERSTACK_BUILD_NAME || 'BStack Build Name';
 const projectName = process.env.BROWSERSTACK_PROJECT_NAME || 'BStack Project Name';
 const localIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER || 'browserstack_local_identifier';
 
-// Input capabilities
+if (!username || !accessKey) {
+  console.error('Missing BrowserStack credentials in env vars');
+  process.exit(1);
+}
+
 const capabilities = {
   os: 'Windows',
   os_version: '10',
@@ -27,7 +30,6 @@ const driver = new webdriver.Builder()
   .withCapabilities(capabilities)
   .build();
 
-// HTTP Server should be running on 8099 port of GitHub runner
 driver.get('http://localhost:8099').then(function () {
   driver.getTitle().then(function (title) {
     console.log(title);
